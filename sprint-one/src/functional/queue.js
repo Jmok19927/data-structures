d324f5193fade43014a195bddb29a2dfb38682d9
Jmok19927 <jmok19927@gmail.com>
2022-06-02 11:50:41 -0700

    finished functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,25 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
+  var currentPosition = 0;
+  var queueFront = 0;
 
   someInstance.enqueue = function(value) {
+    storage[currentPosition] = value;
+    currentPosition++;
   };
 
   someInstance.dequeue = function() {
+    if (currentPosition - queueFront > 0) {
+      var result = storage[queueFront];
+      delete storage[queueFront];
+      queueFront++;
+      return result
+    }
   };
 
   someInstance.size = function() {
+    return currentPosition - queueFront;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,16 +2,28 @@ var Stack = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = {
+  };
 
+  var stackCount = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[stackCount] = value;
+    stackCount++;
   };
 
   someInstance.pop = function() {
+    if (stackCount > 0) {
+      var result = storage[stackCount - 1];
+      delete storage[stackCount - 1];
+      stackCount--;
+      return result;
+    }
+
   };
 
   someInstance.size = function() {
+    return stackCount;
   };
 
   return someInstance;
