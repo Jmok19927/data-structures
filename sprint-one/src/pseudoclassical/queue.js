f2c8fda603db3fe41bcc1931227fe37ba72ff73e
Jmok19927 <jmok19927@gmail.com>
2022-06-02 15:10:49 -0700

    completed pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,27 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.front = 0;
+  this.back = 0;
+
 };
 
+Queue.prototype.size = function() {
+  return this.back - this.front;
+}
+
+Queue.prototype.enqueue = function(value) {
+  this[this.back] = value;
+  this.back++;
+}
+
+Queue.prototype.dequeue = function() {
+  if (this.back - this.front > 0) {
+    var result = this[this.front];
+    delete this[this.front];
+    this.front++;
+    return result;
+  }
+}
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.stackCount = 0;
 };
 
+Stack.prototype.size = function() {
+  return this.stackCount;
+}
+
+Stack.prototype.push = function(value) {
+  this[this.stackCount] = value;
+  this.stackCount++;
+}
+
+Stack.prototype.pop = function() {
+  if (this.stackCount > 0) {
+    var result = this[this.stackCount - 1];
+    delete this[this.stackCount - 1];
+    this.stackCount--;
+    return result;
+  }
+}
 
