f5527f2f21c8122dca102b4c9c4f7f5906b6ade2
Jmok19927 <jmok19927@gmail.com>
2022-06-02 15:17:09 -0700

    finished es6 pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.front = 0;
+    this.back = 0;
   }
+
+  size() {
+    return this.back - this.front;
+  }
+
+  enqueue(value) {
+    this[this.back] = value;
+    this.back++;
+  }
+
+  dequeue() {
+    if (this.back > this.front) {
+      var result = this[this.front];
+      delete this[this.front];
+      this.front++
+      return result;
+    }
+  }
+
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,25 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.stackCount = 0;
+  }
+
+  size() {
+    return this.stackCount;
+  }
+
+  push(value) {
+    this[this.stackCount] = value;
+    this.stackCount++;
+  }
+
+  pop() {
+    if (this.stackCount > 0) {
+      var result = this[this.stackCount - 1];
+      delete this[this.stackCount - 1];
+      this.stackCount--;
+      return result;
+    }
   }
 
 }
