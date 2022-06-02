4b650bc1975aa706a0cddd2f799145a7df4df91a
Jmok19927 <jmok19927@gmail.com>
2022-06-02 12:31:12 -0700

    finished stack for prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var newStack = Object.create(stackMethods);
+  newStack.stackCount = 0;
+
+  return newStack;
 };
 
 var stackMethods = {};
+
+stackMethods.size = function() {
+  return this.stackCount;
+};
+
+stackMethods.push = function(value) {
+  this[this.stackCount] = value;
+  this.stackCount++;
+};
+
+stackMethods.pop = function() {
+  if (this.stackCount > 0) {
+    var result = this[this.stackCount - 1];
+    delete this[this.stackCount - 1];
+    this.stackCount--;
+    return result;
+  }
+};
 
 
