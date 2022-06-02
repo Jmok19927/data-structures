80b3a84512bef9e639a1caa92c8f843294fff533
Jmok19927 <jmok19927@gmail.com>
2022-06-02 12:20:05 -0700

    Completed functional shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var queueObject = {};
+  queueObject.queueFront = 0;
+  queueObject.queueBack = 0;
+  _.extend(queueObject, queueMethods);
+
+  return queueObject;
 };
 
 var queueMethods = {};
+
+queueMethods.size = function() {
+  return this.queueBack - this.queueFront;
+}
+
+queueMethods.enqueue = function(value) {
+  this[this.queueBack] = value;
+  this.queueBack++;
+}
+
+queueMethods.dequeue = function() {
+  if (this.queueBack > this.queueFront) {
+    var result = this[this.queueFront];
+    delete this[this.queueFront];
+    this.queueFront++;
+    return result
+  }
+}
 
 
