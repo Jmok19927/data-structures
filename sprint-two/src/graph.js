dd51e57cfab53c63e78205dd79875c5acfc73342
Jmok19927 <jmok19927@gmail.com>
2022-06-03 13:26:25 -0700

    completed graph and set, progress on hashtable

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,31 +1,32 @@
 // Instantiate a new graph
 var Graph = function() {
-  this.nodes = {}; // an array to hold node objects
+  this.nodes = {}; // empty object
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.nodes[node] = [node];
+  this.nodes[node] = {};
+  this.nodes[node][node] = node;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  for (let key in this.nodes) {
-    if (this.nodes[key]) {
-      return true;
-    }
+  if (this.nodes[node] !== undefined) {
+    return true;
   }
   return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-      delete this.nodes[node];
+  // technically adding the node back in would keep its edges
+  // fix in future to remove the edge reference
+  delete this.nodes[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  if (this.nodes[fromNode].indexOf(toNode) !== -1) {
+  if (this.nodes[fromNode][toNode] && this.nodes[toNode]) { // technically adding the node back in would keep its edges, fix in remove node
     return true;
   }
   return false;
@@ -33,16 +34,22 @@ Graph.prototype.hasEdge = function(fromNode, toNode) {
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  this.nodes[fromNode].push(toNode.nodes[0]);
-  this.nodes[toNode].push(fromNode.nodes[0]);
+  this.nodes[fromNode][toNode] = toNode;
+  this.nodes[toNode][fromNode] = fromNode;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.nodes[fromNode][toNode];
+  delete this.nodes[toNode][fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (let key in this.nodes) {
+    let nodeValue = this.nodes[key][key];
+    cb(nodeValue);
+  }
 };
 
 /*
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,16 +5,49 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function(k, v) { // k = index? v = value?
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var object = {};
+  object[k] = v;
+// if there is no current object at index at all, put this object at that index
+  // if (!this._storage.get(index)) {
+  //   this._storage.set(index, object);
+  // } else if (Object.keys(this._storage.get(index)[0]) === k){
+  //   this._storage.set(index, object);
+  // } else {
+  //   while (this._storage.get(index)) {
+  //     index++;
+  //   }
+  //   this._storage.set(index, v);
+  // }
+
+  if (this._storage.get(index) === undefined) {
+    this._storage.set(index, object);
+  } else {
+    if (Object.keys(this._storage.get(index)[0]) === k) {
+      this._storage.set(index, object);
+    } else {
+      while (this._storage.get(index)) {
+        index++;
+      }
+      this._storage.set(index, object);
+    }
+  }
+
+
+// if there IS an object at the index, check if k matches, if it does replace v
+// if there is an object and k does not match, incredment index
+
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
