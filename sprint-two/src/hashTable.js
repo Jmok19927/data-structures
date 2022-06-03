041d5ec67925bc42d9bfe03343c9c8352e2484ff
Jmok19927 <jmok19927@gmail.com>
2022-06-03 15:44:22 -0700

    finished basic requirements

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,24 +7,13 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) { // k = index? v = value?
   var index = getIndexBelowMaxForKey(k, this._limit);
-  var object = {};
+  var object = {}; // can prob turn this into a tuple array instead if we cared
   object[k] = v;
-// if there is no current object at index at all, put this object at that index
-  // if (!this._storage.get(index)) {
-  //   this._storage.set(index, object);
-  // } else if (Object.keys(this._storage.get(index)[0]) === k){
-  //   this._storage.set(index, object);
-  // } else {
-  //   while (this._storage.get(index)) {
-  //     index++;
-  //   }
-  //   this._storage.set(index, v);
-  // }
 
   if (this._storage.get(index) === undefined) {
     this._storage.set(index, object);
   } else {
-    if (Object.keys(this._storage.get(index)[0]) === k) {
+    if (Object.keys(this._storage.get(index))[0] === k) { // changed [0] to be outside of object keys fixing undefined issue
       this._storage.set(index, object);
     } else {
       while (this._storage.get(index)) {
@@ -35,18 +24,29 @@ HashTable.prototype.insert = function(k, v) { // k = index? v = value?
   }
 
 
-// if there IS an object at the index, check if k matches, if it does replace v
+// if there IS an object at the index, check if k matches, if it does replace
 // if there is an object and k does not match, incredment index
 
 };
 
 HashTable.prototype.retrieve = function(k) {
+  // need to add a check if this._storage.get(index) exists
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index)[k];
+  // need to increment the index until we find the matching key if the first key does not match
+  if (this._storage.get(index)) {
+    while (Object.keys(this._storage.get(index))[0] !== k) {
+      index++;
+    }
+    return this._storage.get(index)[k];
+  }
+  return undefined;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  while (Object.keys(this._storage.get(index))[0] !== k) {
+    index++;
+  }
   this._storage.set(index, undefined);
 };
 
